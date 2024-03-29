303143a3cbc3688ed6ce76dd65ca9978dd6c3315
PaulKMueller <paulkonstantinmueller@gmail.com>
2024-03-29 15:02:36 +0100

    Working Mentor Ranking Submission

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, redirect, url_for, flash
-from forms import MentorForm, MenteeForm
+from forms import MentorForm, MenteeForm, MentorRatingForm
 from flask_sqlalchemy import SQLAlchemy
 import qrcode
 from flask import send_file
 from io import BytesIO
+from wtforms import StringField, IntegerField, TextAreaField, SubmitField, SelectField
+from wtforms.validators import DataRequired, NumberRange, Optional
 
 
 app = Flask(__name__)
@@ -56,16 +58,41 @@ def generate_qr():
     img_io.seek(0)
     return send_file(img_io, mimetype='image/jpeg')
 
+@app.route('/confirmation_page')
+def confirmation_page():
+    return "<p>Thank you for your entries!</p>"
+
+
 @app.route('/mentee_form', methods=['GET', 'POST'])
 def mentee_form():
     form = MenteeForm()
-    if form.validate_on_submit():
-        # Process form data here, such as saving the mentee's ratings
-        flash('Thank you for submitting your preferences!', 'success')
-        return redirect(url_for('mentee_form'))
-    return render_template('mentee_form.html', form=form)
+    mentors = Mentor.query.all()
+    
+    if not form.is_submitted():
+        form.mentor_ratings.min_entries = len(mentors)
+        for mentor in mentors:
+            mentor_form = MentorRatingForm()
+            mentor_form.rating.label = f'Rating for {mentor.name}'
+            form.mentor_ratings.append_entry(mentor_form)
+    else:
+        # If the form has been submitted, we process the form
+        if form.validate_on_submit():
+            # Create a new Mentee instance
+            mentee = Mentee(name=form.name.data)
+            db.session.add(mentee)
+            db.session.commit()  # Commit to get the mentee ID
 
+            for mentor_form, mentor in zip(form.mentor_ratings.entries, mentors):
+                rating_value = mentor_form.rating.data
+                if rating_value:  # Assuming 0 means no rating given
+                    rating = Rating(mentee_id=mentee.id, mentor_id=mentor.id, rating=rating_value)
+                    db.session.add(rating)
 
+            db.session.commit()
+            flash('Preferences submitted successfully!', 'success')
+            return redirect(url_for('confirmation_page'))  # Redirect to a confirmation page or some other page
+
+    return render_template('mentee_form.html', form=form, mentors=mentors)
 
 
 class Mentor(db.Model):
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, NumberRange
+from flask_wtf import FlaskForm, Form
+from wtforms import StringField, IntegerField, TextAreaField, SubmitField, SelectField, FormField, FieldList
+from wtforms.validators import DataRequired, NumberRange, Optional
 
 class MentorForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -8,7 +8,15 @@ class MentorForm(FlaskForm):
     timeslots = StringField('Timeslots (comma-separated, e.g., 9am-10am, 10am-11am)', validators=[DataRequired()])
     submit = SubmitField('Register Mentor')
 
+class MentorRatingForm(FlaskForm):
+    rating = SelectField(
+        'Rating',
+        choices=[('0', 'Select a ranking')] + [(str(i), str(i)) for i in range(1, 7)],
+        validators=[Optional()],
+        coerce=int
+    )
+
 class MenteeForm(FlaskForm):
     name = StringField('Your Name', validators=[DataRequired()])
-    # Add other fields as necessary, such as ratings for each mentor
-    submit = SubmitField('Submit')
+    mentor_ratings = FieldList(FormField(MentorRatingForm), min_entries=1)
+    submit = SubmitField('Submit Preferences')
