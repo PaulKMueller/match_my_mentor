2f34af5d8210429369dc8e5de92139060339fb1c
PaulKMueller <paul.mueller@quantco.com>
2024-04-06 13:21:16 +0200

    Working Name Validation for Mentors

--- forms.py
+++ forms.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm, Form
 from wtforms import StringField, IntegerField, TextAreaField, SubmitField, SelectField, FormField, FieldList
 from wtforms.validators import DataRequired, NumberRange, Optional, ValidationError
-from models import Mentee
+from models import Mentee, Mentor
 
 class MentorForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     job_description = TextAreaField('Job Description', validators=[DataRequired()])
     timeslots = StringField('Timeslots (comma-separated, e.g., 9am-10am, 10am-11am)', validators=[DataRequired()])
     submit = SubmitField('Register Mentor')
+
+    def validate_name(self, field):
+        if Mentor.query.filter_by(name=field.data).first():
+            raise ValidationError('This name already exists in the database.')
 
 class MentorRatingForm(FlaskForm):
     rating = SelectField(
