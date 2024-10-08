07d97dc3e5c2856e7d901aa52471116d1cfe5de6
PaulKMueller <paul.mueller@quantco.com>
2024-10-08 11:56:41 +0200

    Make database more robust

--- src/models.py
+++ src/models.py
@@ -4,32 +4,42 @@ from sqlalchemy import event
 db = SQLAlchemy()
 
 mentor_timeslot = db.Table('mentor_timeslot',
-    db.Column('mentor_id', db.Integer, db.ForeignKey('mentor.id'), primary_key=True),
-    db.Column('timeslot_id', db.Integer, db.ForeignKey('time_slot.id'), primary_key=True)
+    db.Column('mentor_id', db.Integer, db.ForeignKey('mentor.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('timeslot_id', db.Integer, db.ForeignKey('time_slot.id', ondelete='CASCADE'), primary_key=True)
 )
 
 class Mentor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     job_description = db.Column(db.Text, nullable=False)
-    timeslots = db.relationship('TimeSlot', secondary=mentor_timeslot, lazy='dynamic', back_populates='mentors')
+    timeslots = db.relationship(
+        'TimeSlot',
+        secondary=mentor_timeslot,
+        back_populates='mentors',
+        cascade="all, delete"
+    )
+    ratings = db.relationship('Rating', cascade="all, delete", backref='mentor')
 
 class Mentee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    ratings = db.relationship('Rating', backref='mentee', lazy=True)
+    ratings = db.relationship('Rating', cascade="all, delete", backref='mentee')
 
 class Rating(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    mentee_id = db.Column(db.Integer, db.ForeignKey('mentee.id'), nullable=False)
-    mentor_id = db.Column(db.Integer, db.ForeignKey('mentor.id'), nullable=False)
+    mentee_id = db.Column(db.Integer, db.ForeignKey('mentee.id', ondelete='CASCADE'), nullable=False)
+    mentor_id = db.Column(db.Integer, db.ForeignKey('mentor.id', ondelete='CASCADE'), nullable=False)
     rating = db.Column(db.Integer, nullable=False, default=6)
 
 class TimeSlot(db.Model):
     id = db.Column(db.String, primary_key=True)
     start_time = db.Column(db.Time, nullable=False)
     end_time = db.Column(db.Time, nullable=False)
-    mentors = db.relationship('Mentor', secondary=mentor_timeslot, lazy='dynamic', back_populates='timeslots')
+    mentors = db.relationship(
+        'Mentor',
+        secondary=mentor_timeslot,
+        back_populates='timeslots'
+    )
 
 # Function to create default ratings for all mentees when a new mentor is added
 def create_default_ratings_for_new_mentor(mapper, connection, target):
